244de118858d473b4058d81513a89e0a47fa5db6
Itzhak Levy <itzhaklevydev@gmail.com>
2025-03-19 17:08:16 +0200

    Add the devtools dependency to the pom.xml file for automatic reloading on change and add additional basic end point mapping

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -5,10 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // Expose "/" that return "Hello World"
 
+    // Expose "/" that return "Hello World"
     @GetMapping("/")
-    public String sayHello() {
+    public String sayHello() { //
         return "Hello World!";
+    }
+
+    // Expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // Expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
     }
 }
